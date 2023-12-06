9e6eaf76ddcc907d0ef170e7e4d4c6e699b03bac
le402n28\uer <linyier1027@gmail.com>
2023-12-06 16:48:39 +0800

    使用rvest擷取網頁資訊

--- 1206.R
+++ 1206.R
@@ -66,3 +66,12 @@ NTU_info = function () {
 }
 
 NTU_info()
+
+library(rvest)
+
+URL = "https://reg.ntuh.gov.tw/EmgInfoBoard/NTUHEmgInfo.aspx"
+
+website = read_html(URL)
+
+needed_txt = website %>% html_nodes("tr") %>% html_text()
+needed_txt
